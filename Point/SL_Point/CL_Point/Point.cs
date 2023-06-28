b95b69f6b7db7bdbe7733097b1768417444cfc3f
fmemeteau <131686141+fmemeteau@users.noreply.github.com>
2023-06-28 09:40:49 +0200

    Create_Point

--- Point/SL_Point/CL_Point/Point.cs
+++ Point/SL_Point/CL_Point/Point.cs
@@ -1,7 +1,67 @@
-﻿namespace CL_Point
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_Point
 {
     public class Point
     {
+        float x, y;
 
+        // Les 2 constructeurs classiques (sans celui de copie)
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        // Copier un point existant afin de créer un point symétrique selon l'axe des abscisses (z = 'a'), des ordonnées(z = 'o')
+        // ou l'origine (z = '0')
+        public Point(Point pointACopierAxeOrdonnees, char z)
+        {
+            if (z == 'o')
+            {
+                this.x = pointACopierAxeOrdonnees.x - 2 * pointACopierAxeOrdonnees.x;
+                this.y = pointACopierAxeOrdonnees.y;
+            }
+
+            else if (z == 'a')
+            {
+                this.x = pointACopierAxeOrdonnees.x;
+                this.y = pointACopierAxeOrdonnees.y - 2 * pointACopierAxeOrdonnees.y;
+            }
+            
+            else if (z == '0')
+            {
+                this.x = pointACopierAxeOrdonnees.x - 2 * pointACopierAxeOrdonnees.x;
+                this.y = pointACopierAxeOrdonnees.y - 2 * pointACopierAxeOrdonnees.y;
+            }
+            else
+            {
+                throw new Exception("Veuillez saisir 'a' pour abscisses , 'o' pour ordonnées ou '0' pour l'origine !");
+            }
+        }
+
+        public void Permutation(Point point)
+        {
+            float buffer = point.x;
+            point.x = point.y;
+            point.y = buffer;
+        }
+
+        public string AfficherCoordonnees()
+        {
+            return "Le point x se situe en " + this.x + ".\n" + "Le point y se situe en " + this.y + ".";
+        }
     }
 }
--- Point/SL_Point/PR_Point/Program.cs
+++ Point/SL_Point/PR_Point/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace PR_Point
+﻿using CL_Point;
+//using System.Drawing;
+
+
+
+namespace PR_Point
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Point p1 = new Point(7, 15);
+
+            Point p2 = new Point(p1, '0');
+
+            Console.WriteLine(p1.AfficherCoordonnees() + "\n");
+            Console.WriteLine(p2.AfficherCoordonnees() + "\n");
+
+            p1.Permutation(p1);
+
+            Console.WriteLine(p1.AfficherCoordonnees() + "\n");
+
         }
     }
 }
